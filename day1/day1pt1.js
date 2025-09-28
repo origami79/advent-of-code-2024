3f0331a2b14b5f8bd557207de9882c35916253d7
Sarah <cerezo.sarah@gmail.com>
2025-09-28 11:13:07 -0400

    day 1

---
+++ day1/day1pt1.js
@@ -0,0 +1,32 @@
+// take two arrays of numbers
+// sort each one from smallest to largest
+// for each index, subtract the number at that index in each list to find the difference and add that number to a counter
+// return that counter
+
+let array1 = []
+let array2 = []
+let distance = 0
+
+function parseInput() {
+  const fs = require('fs');
+  
+  data = fs.readFileSync('day1/input.txt')
+  const stringArray = data.toString().split("\r\n");
+  stringArray.forEach((line) => {
+    let pair = line.split("   ")
+    array1.push(parseInt(pair[0]))
+    array2.push(parseInt(pair[1]))
+  })
+}
+
+parseInput()
+array1 = array1.sort()
+array2 - array2.sort()
+// console.log({array1, array2})
+
+for (let i = 0; i < array1.length; i++) {
+  let difference = Math.abs(array1[i] - array2[i])
+  distance += difference
+}
+
+console.log({distance})
